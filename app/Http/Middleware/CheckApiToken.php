5e194ee847bf19dd5a6943134801cb3292769256
Jorge <georgiearclopez@gmail.com>
2025-02-12 20:19:33 +0100

    cambios pequeós

--- app/Http/Middleware/CheckApiToken.php
+++ app/Http/Middleware/CheckApiToken.php
@@ -4,20 +4,16 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Session;
 
 class CheckApiToken
 {
     public function handle(Request $request, Closure $next)
     {
-        $token = session('api_token');
-        
-        if (!$token) {
-            return response()->json(['error' => 'No autorizado'], 401);
+        if (!session()->has('api_token')) {
+            return redirect()->route('login')
+                ->with('error', 'Por favor inicia sesión para continuar.');
         }
-        // Agregar el token a las cabeceras de todas las peticiones
-        $request->headers->set('Authorization', 'Bearer ' . $token);
-        
+
         return $next($request);
     }
 }
