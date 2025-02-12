4b583de8450e2da54b40b7813310d27245091f88
ErDekas <pabletor0505@gmail.com>
2025-02-12 19:50:18 +0100

    Cambio en login

--- app/Http/Requests/RegisterUserRequest.php
+++ app/Http/Requests/RegisterUserRequest.php
@@ -13,10 +13,11 @@ class RegisterUserRequest extends FormRequest
 
     public function rules()
     {
+        dd($this->all());        
         return [
             'name' => 'required|string|max:255',
             'email' => 'required|email|unique:users',
-            'password' => 'required|min:8|confirmed',
+            'password' => 'required|min:6|confirmed',
             'tipo_inscripcion' => 'required|in:virtual,presential,free', // Estos valores deben coincidir con registration_type
             'certificado_alumno' => 'required_if:tipo_inscripcion,free|boolean'
         ];
