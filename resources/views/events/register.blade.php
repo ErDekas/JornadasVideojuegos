a9296a59280a2ce8cc4ac5fcc5e2306d327fb9a9
Jorge <georgiearclopez@gmail.com>
2025-02-13 17:31:47 +0100

    Pequeños cambios

--- resources/views/events/register.blade.php
+++ resources/views/events/register.blade.php
@@ -1,22 +1,27 @@
 @extends('layouts.app')
+
 @section('content')
 <div class="row justify-content-center">
     <div class="col-md-6">
         <div class="card">
             <div class="card-body">
                 <h1 class="card-title">Registro para {{ $event['event']['title'] }}</h1>
-                <form action="{{ route('events.register.submit', $event['event']['id']) }}" method="POST">
-                    @csrf
-                    <div class="mb-3">
-                        <label class="form-label">Nombre</label>
-                        <input type="text" name="name" class="form-control" required>
+
+                @if(session('success'))
+                    <div class="alert alert-success">
+                        {{ session('success') }}
                     </div>
-                    <div class="mb-3">
-                        <label class="form-label">Email</label>
-                        <input type="email" name="email" class="form-control" required>
+                @elseif(session('error'))
+                    <div class="alert alert-danger">
+                        {{ session('error') }}
                     </div>
-                    <button type="submit" class="btn btn-primary w-100">Confirmar registro</button>
-                </form>
+                @endif
+
+                @//if()
+                    <div class="alert alert-success">
+                        Ya estás registrado en este evento.
+                    </div>
+                @//endif
             </div>
         </div>
     </div>
